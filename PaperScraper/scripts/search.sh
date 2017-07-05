a70c115a3f49825301ef6630550f4415fabc5e4c
Sean Kugele <seankugele@gmail.com>
2017-07-05 20:16:17 +0000

    Updated search.sh to accept and argument for search criteria.

--- PaperScraper/scripts/search.sh
+++ PaperScraper/scripts/search.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-find -iname '*.pdf' -exec sh -c 'pdftotext "{}" - 2>/dev/null | grep --with-filename --label="{}" --color "internal"' \;
-find -iname '*.doc' -exec sh -c 'catdoc "{}" 2>/dev/null | grep --with-filename --label="{}" --color "action"' \;
+find -iname '*.pdf' -exec sh -c "pdftotext '{}' - 2>/dev/null | grep --with-filename --label='{}' --color '$1'" \;
+find -iname '*.doc' -exec sh -c "catdoc '{}' 2>/dev/null | grep --with-filename --label='{}' --color '$1'" \;
